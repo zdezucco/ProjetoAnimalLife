ebd0633f0610970de1147f23e8bdec847bd93f2e
Alessandra Estephany <alessandraestph@gmail.com>
2025-02-23 17:55:42 -0300

    1° Comcitr ciação repositório

---
+++ src/main/java/org/example/backendanimallife/BackEndAnimalLifeApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendanimallife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAnimalLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndAnimalLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/backendanimallife/BackEndAnimalLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendanimallife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAnimalLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
