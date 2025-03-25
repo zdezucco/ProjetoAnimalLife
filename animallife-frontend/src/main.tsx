25848a694a2b3ec3a7af49b6e3064befb21636eb
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-03-24 22:37:03 -0300

    Tela de Variáveis de cores
    Foi criado uma tela com todas as variáveis de cores para que possamos dar sequencia no andamento do projeto.

--- animallife-frontend/src/main.tsx
+++ animallife-frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './App.tsx'
+import Color from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Color />
   </StrictMode>,
 )
