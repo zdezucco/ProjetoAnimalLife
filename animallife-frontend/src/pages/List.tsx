6b86afcde3765700bdc8745331b9954673401a71
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-05-14 20:16:52 -0300

    teste

--- animallife-frontend/src/pages/List.tsx
+++ animallife-frontend/src/pages/List.tsx
@@ -47,12 +47,12 @@ export default function AnimalList() {
   return (
     <div className="TelaAnimal">
       <div id="background-list">
+      <div id="top-header">
+        <h1 className="header">Animais</h1>
+      </div>
       <div className="notification">
             <Bell className="bell-icon" />
             <span className="notification-count">2</span>
-      </div>
-      <div id="top-header">
-        <h1 className="header">Animais</h1>
       </div>
 
         <div id="search-container">
