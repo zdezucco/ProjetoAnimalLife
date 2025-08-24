f850a1959bc05608b55ce82d96eb3db9e9a7557c
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-08-24 18:44:55 -0300

    Update List.tsx

--- animallife-frontend/src/pages/List.tsx
+++ animallife-frontend/src/pages/List.tsx
@@ -8,8 +8,8 @@ import OrangeTemp from "../assets/orange-term.svg";
 import GrayTemp from "../assets/gray-term.svg";
 import { useNavigate } from "react-router";
 import FooterBar from "../components/FooterBar";
-import FemaleIcon from "@/assets/female-icon.png";
-import MaleIcon from "@/assets/male-icon.png";
+import FemaleIcon from "../assets/female-icon.png";
+import MaleIcon from "../assets/male-icon.png";
 
 
 
