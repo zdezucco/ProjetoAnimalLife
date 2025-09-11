40095771f29a2905c52d690d0bdc6e9131f27518
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-09-10 22:29:57 -0300

    Update App.tsx

--- animallife-frontend/src/App.tsx
+++ animallife-frontend/src/App.tsx
@@ -5,7 +5,6 @@ import Monitoramento from "./pages/Monitoramento";
 import RecuperaSenha from "./pages/Recuperasenha";
 import NovaSenha from "./pages/Novasenha";
 import LoadingScreen from "./pages/Loadinscreen";
-import Monitoramento2 from "./pages/Monitoramento2";
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<Login />}/>
         <Route path="/List" element={<List />} />
         <Route path="/Monitoramento" element={<Monitoramento />}/>
-        <Route path="/Monitoramento2" element={<Monitoramento2 />}/>
         <Route path="/RecuperaSenha" element={<RecuperaSenha />}/>
         <Route path="/Novasenha" element={<NovaSenha />}/> 
         <Route path="/LoadingScreen" element={<LoadingScreen />}/> 
