0957f4b46265cd9985d87c9b89af87c774000119
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-10-12 14:46:56 -0300

    Update List.tsx

--- animallife-frontend/src/pages/List.tsx
+++ animallife-frontend/src/pages/List.tsx
@@ -45,10 +45,14 @@ const getStatusIcon = (temp: number | undefined) => {
 };
 
 const getStatusColor = (temp: number | undefined) => {
-  if (temp === undefined) return "#757575";
-  if (temp < 36) return "#E53935";
-  if (temp > 40) return "#FF9800";
-  return "#4CAF50";
+  if (temp === undefined ) return GrayTemp;
+  if (temp === 0) return GrayTemp;
+  if (temp === null) return GrayTemp;
+  if (temp <= 34) return RedTemp;
+  if (temp <= 36) return OrangeTemp;
+  if (temp >= 40) return OrangeTemp;
+  if (temp >= 41) return RedTemp;
+  return GreenTemp;
 };
 
 export default function AnimalList() {
