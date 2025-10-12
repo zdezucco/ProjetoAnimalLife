a9da4249dbe6484c6068572b61a1f168d89f677c
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-10-12 15:02:37 -0300

    Update List.tsx

--- animallife-frontend/src/pages/List.tsx
+++ animallife-frontend/src/pages/List.tsx
@@ -37,8 +37,8 @@ const getStatusIcon = (temp: number | undefined) => {
   if (temp === undefined ) return GrayTemp;
   if (temp === 0) return GrayTemp;
   if (temp === null) return GrayTemp;
-  if (temp <= 34) return RedTemp;
-  if (temp <= 36 && temp >= 34.1) return OrangeTemp;
+  if (temp <= 35) return RedTemp;
+  if (temp <= 36 && temp >= 35.1) return OrangeTemp;
   if (temp >= 40 && temp <= 41) return OrangeTemp;
   if (temp >= 41.1) return RedTemp;
   return GreenTemp;
@@ -48,8 +48,8 @@ const getStatusColor = (temp: number | undefined) => {
   if (temp === undefined ) return 'var(--gray-temp)';
   if (temp === 0) return 'var(--gray-temp)';
   if (temp === null) return 'var(--gray-temp)';
-  if (temp <= 34) return 'var(--red-temp)';
-  if (temp <= 36 && temp >= 34.1) return 'var(--orange-temp)';
+  if (temp <= 35) return 'var(--red-temp)';
+  if (temp <= 36 && temp >= 35.1) return 'var(--orange-temp)';
   if (temp >= 40 && temp <= 41) return 'var(--orange-temp)';
   if (temp >= 41) return 'var(--red-temp)';
   return 'var(--green-temp)';
