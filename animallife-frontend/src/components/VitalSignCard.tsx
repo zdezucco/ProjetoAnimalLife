d06137046acd97d2e3292a8a9ae6d6bdddcc1ee0
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-11-08 01:26:37 -0300

    Update VitalSignCard.tsx

--- animallife-frontend/src/components/VitalSignCard.tsx
+++ animallife-frontend/src/components/VitalSignCard.tsx
@@ -88,7 +88,7 @@ const VitalSignCard: React.FC<VitalSignCardProps> = ({ monitoramentos }) => {
       <CardHeader style={{ backgroundColor: color }}>Temperatura:</CardHeader>
       <CardContent>
         <LeftSection>
-          <img src={icon} alt="Ícone Termômetro" width={30} height={30} />
+          <img src={icon} alt="Ícone Termômetro" width={34} height={34} />
           <TemperateMedium>
             <Temperature style={{ color: textcolor }}>{temp.toFixed(1)}°c</Temperature>
             <Average style={{ color: textcolor }}>Média: {media.toFixed(1)}°c</Average>
