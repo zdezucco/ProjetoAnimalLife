15eaf12cf3e4eee891f942964b2f2a4c6456eede
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-11-20 03:59:35 -0300

    Update Header.tsx

--- animallife-frontend/src/components/Header.tsx
+++ animallife-frontend/src/components/Header.tsx
@@ -243,8 +243,6 @@ const Header = ({ selectedId }: HeaderProps) => {
 
   const openFilePicker = () => fileInputRef.current?.click();
 
-  const openPreview = () => setShowPreview(true);
-
   return (
     <>
       <div className="header-container">
@@ -262,7 +260,7 @@ const Header = ({ selectedId }: HeaderProps) => {
 
       {/* FOTO DO ANIMAL */}
       <div className="animal-photo-section">
-        <div className="photo-wrapper" onClick={() => {openPreview}}>
+        <div className="photo-wrapper" onClick={() => setShowPreview(true)}>
           <img
             src={selectedAnimal?.avatar || "/avatars/default.png"}
             alt="Animal"
