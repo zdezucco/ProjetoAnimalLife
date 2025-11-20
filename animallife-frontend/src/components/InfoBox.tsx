a845d29aa925851c256518cd2f80a0bda40614c3
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2025-11-20 03:51:31 -0300

    Update InfoBox.tsx

--- animallife-frontend/src/components/InfoBox.tsx
+++ animallife-frontend/src/components/InfoBox.tsx
@@ -252,7 +252,7 @@ const InfoBox = ({ animalId }: InfoBoxProps) => {
           <div className="form-row">
             <div className="form-group">
               <label htmlFor="idade">Idade:</label>
-              <input id="idade" type="text" value={formData.idade || ""} readOnly > Anos</input>
+              <input id="idade" type="text" value={formData.idade ? `${formData.idade} Anos` : ""} readOnly />
             </div>
 
             <div className="form-group">
